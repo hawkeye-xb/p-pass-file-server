bcddff112dcccb4b41fb7aac81e8376dddbd6bdd
lizhaowen <690591397@qq.com>
2025-01-02 14:43:02 +0800

    move src

--- src/controllers/move.ts
+++ src/controllers/move.ts
@@ -1,11 +1,52 @@
 import fs from 'fs-extra';
 import Joi from "joi";
 import path from "path";
-import { Context } from "../../types/index.ts";
-import { joiValidate, handleErrorMessage, ERROR_STATUS, ERROR_CODE ,ERROR_MSG, handleTryCatchError } from '../utils/index.ts';
+import { Context } from "../types/index.ts";
+import { joiValidate, handleErrorMessage, handleTryCatchError, RULES_ERR } from './utils/index.ts';
 
-const moveDirSchema = Joi.object({
-  src: Joi.string().required(),
+const moveSrcSchema = Joi.object({
+  src: Joi.array().items(Joi.string()).required(),
   dest: Joi.string().required(),
 });
-export const moveDir = (ctx: Context) => {}
+export const moveSrc = (ctx: Context) => {
+  try {
+    const value = joiValidate(moveSrcSchema, ctx);
+    if (value === null) {
+      return;
+    }
+
+    const { src, dest } = value;
+    if (!fs.existsSync(dest)) {
+      handleErrorMessage(ctx, RULES_ERR.FS_EXIST_SYNC);
+      return;
+    }
+    if (!fs.statSync(dest).isDirectory()) {
+      handleErrorMessage(ctx, RULES_ERR.FS_STAT_SYNC);
+      return;
+    }
+
+    for (const target of src) {
+      if (!fs.existsSync(target)) {
+        handleErrorMessage(ctx, RULES_ERR.FS_EXIST_SYNC);
+        return;
+      }
+
+      const targetDestPath = path.join(dest, path.basename(target));
+      if (fs.existsSync(targetDestPath)) {
+        handleErrorMessage(ctx, RULES_ERR.FS_EXIST_SYNC);
+        return;
+      }
+    }
+
+    for (const target of src) {
+      const targetDestPath = path.join(dest, path.basename(target));
+      fs.moveSync(target, targetDestPath);
+    }
+    ctx.body = {
+      code: 0,
+      message: 'success',
+    };
+  } catch (error) {
+		handleTryCatchError(ctx, error);
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import Router from "koa-router";
 import dirRouter from "./dir.ts";
+import { moveSrc } from "@/controllers/move.ts";
 
 const router = new Router();
 
@@ -8,5 +9,7 @@ router.get("/", async (ctx) => {
 });
 
 router.use("/dir", dirRouter.routes(), dirRouter.allowedMethods());
+
+router.post("/move", moveSrc)
 
 export default router;
